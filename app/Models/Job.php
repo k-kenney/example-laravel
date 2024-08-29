f60e72fce525766e09e920bb0ec24944d10decf2
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-08-29 12:29:06 -0500

    tinkered with db

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,6 +8,11 @@ class Job extends Model {
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer() {
+        
+        return $this->belongsTo(employer::class);
+    } 
     
 };
 
