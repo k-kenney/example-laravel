113f4474159ab0ec74ace890b992f9a7859ae8fb
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-08-29 17:31:17 -0500

    added to db

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -10,9 +10,14 @@ class Job extends Model {
     protected $fillable = ['title', 'salary'];
 
     public function employer() {
-        
-        return $this->belongsTo(employer::class);
+
+        return $this->belongsTo(Employer::class);
     } 
     
+
+    public function tags() 
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
+    }
 };
 
