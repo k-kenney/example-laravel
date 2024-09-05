7eb2230e974fcd049dc2324ff72a8f12eda05897
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-09-05 10:31:50 -0500

    added forms

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -1,10 +1,11 @@
 <x-layout>
     <x-slot:heading>
-        Edit Job {{ $job->title }}
+        Edit Job: {{ $job->title }}
     </x-slot:heading>
 
-    <form method="POST" action="/jobs">
+    <form method="POST" action="/jobs/{{ $job->id }}">
         @csrf
+        @method('PATCH')
 
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
@@ -55,14 +56,23 @@
             </div>
         </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
-            <a href="/jobs/{{ $job->id }}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
-
-            <div>
-                <button type="submit"
-                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+        <div class="mt-6 flex items-center justify-between gap-x-6">
+            <div class="flex items-center">
+                <button class="text-red-500 text-sm font-bold" form="delete-form">Delete</button>
+            </div>
+            <div class="flex items-center gap-x-6">
+                <a href="/jobs/{{ $job->id }}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+                <div>
+                    <button type="submit"
+                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+                </div>
             </div>
         </div>
+    </form>
+
+    <form method="POST" action="/jobs/{{ $job->id }}" id="delete-form" class="hidden" >
+        @csrf
+        @method('DELETE')
     </form>
 
 
